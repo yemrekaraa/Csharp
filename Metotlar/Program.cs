daec05a053a228ebd960bb1f31e53f436d48f916
LENOVO <LENOVO@DESKTOP-0571GCA>
2023-09-20 21:34:00 +0300

    Proje dosyası ekle.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Dont repeat yourself - DRY - Clean Code - Best Practice
+
+using Metotlar;
+
+
+Urun urun1 = new Urun();
+urun1.Adi = "Elma";
+urun1.Fiyati = 25;
+urun1.Aciklama = "Amasya";
+
+Urun urun2 = new Urun();
+urun2.Adi = "Kalem";
+urun2.Fiyati = 45;
+urun2.Aciklama = "Adel Kalem";
+
+Urun urun3 = new Urun();
+urun3.Adi = "İncir";
+urun3.Fiyati = 50;
+urun3.Aciklama = "Taze İncir";
+
+Urun[] urunler=new Urun[] {urun1, urun2, urun3};
+
+foreach(Urun urun in urunler) 
+{
+    Console.WriteLine(urun.Adi);
+    Console.WriteLine(urun.Aciklama);
+    Console.WriteLine(urun.Fiyati);
+    Console.WriteLine("----------------");
+}
+
+Console.WriteLine("------Metotlar--------");
+//instance - örnek
+//encapsulation
+SepetManager sepetManeager = new SepetManager();
+sepetManeager.Ekle(urun1);
+sepetManeager.Ekle(urun2);
+
+sepetManeager.Ekle2("Elma", "Amasya Elması", 25);
+sepetManeager.Ekle2("Kalem", "Adel Kalem", 45);
+sepetManeager.Ekle2("İncir", "Taze İncir", 50);
---
+++ donguler/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//aray-dizi
+string[] kurslar = new string[] {"Yazılım geliştirme kampı",
+    "Programlamaya başlangıç için temel kurslar","java","python" }; 
+
+for (int i = 0; i <kurslar.Length; i++)
+{
+    Console.WriteLine(kurslar[i]);
+}
+
+Console.WriteLine("for bitti");
+//foreach ile dizi yazımı
+foreach (string kurs in kurslar)
+{
+    Console.WriteLine(kurs);
+}
+
+Console.WriteLine("sayfa sonu - footor");
+Console.ReadLine();
